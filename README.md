cf494330ffc637d97dda88dd9e4f15f815948993
Primexz <johann@pepebot.info>
2023-02-02 13:33:16 +0100

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# GTAV Script Patcher
+This is a very simple application to patch the script version of GTA Scripts.
+I use this application to patch my scripts for [this repo](https://github.com/Primexz/GTAV-Decompiled-Scripts).
+
+## Prerequisites
+This application is written in Javascript and accordingly requires Node-JS
+
+## Usage
+
+1. copy your scripts into the ``scripts`` folder
+2. ``node ./index.js``
+3. after the program has run, you will find the edited scripts again from the **scriüts** folder.
---
+++ index.js
@@ -0,0 +1,22 @@
+const { readFile, readdir, writeFile } = require('fs/promises');
+
+function intToHex(int) {
+    return "0x" + int.toString(16)
+}
+
+(async() => {
+    const patchedByte = 0xC
+    const files = await readdir("scripts");
+
+    for await ( const file of files ) {
+        const path = `scripts/${file}`
+        const buf = await readFile(path)
+
+        const originalByte = intToHex(buf[4])
+        buf[4] = patchedByte
+
+        await writeFile(path, buf)
+
+        console.log(`Patched script ${file} ${originalByte} => ${intToHex(patchedByte)}`)
+    }
+})()
